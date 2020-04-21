05ec85d729a8e12bb505b8240353f55c09f2ed4f
Robert Keffury <bobby.keffury@me.com>
2020-04-20 19:05:52 -0700

    Added SignUpVC File

---
+++ ExpressWash/ExpressWash/SignUpViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SignUpViewController.swift
+//  ExpressWash
+//
+//  Created by Bobby Keffury on 4/20/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    //MARK: - Properties
+    
+    //MARK: - Outlets
+    
+    //MARK: - Views
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - Methods
+    
+    //MARK: - Actions
+    
+    //MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+}
