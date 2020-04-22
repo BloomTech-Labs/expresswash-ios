fb2ac346fa72722b75fef1765c0b58df336c5f83
Robert Keffury <bobby.keffury@me.com>
2020-04-21 19:42:46 -0700

    Added Secure Text Entry.

--- ExpressWash/ExpressWash/SignUpViewController.swift
+++ ExpressWash/ExpressWash/SignUpViewController.swift
@@ -27,9 +27,46 @@ class SignUpViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupSubviews()
     }
     
     //MARK: - Methods
+    
+    func setupSubviews() {
+        nameTextField.underline()
+        emailAddressTextField.underline()
+        passwordTextField.underline()
+        addButton(to: passwordTextField)
+        confirmTextField.underline()
+        addButton(to: confirmTextField)
+        
+        backgroundView.layer.cornerRadius = 20.0
+    }
+    
+    func addButton(to textField: UITextField) {
+        
+        let button = UIButton(type: .custom)
+        
+        if textField.isSecureTextEntry {
+            button.setTitle("●",for: .normal)
+            button.titleLabel!.textColor = UIColor.init(named: "Salmon")
+        } else {
+            button.setTitle("○",for: .normal)
+            button.titleLabel!.textColor = UIColor.init(named: "Salmon")
+        }
+        
+        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: -16, bottom: 0, right: 0)
+        button.frame = CGRect(x: CGFloat(textField.frame.size.width - 25), y: CGFloat(5), width: CGFloat(25), height: CGFloat(25))
+        button.addTarget(self, action: #selector(self.unhide), for: .touchUpInside)
+        textField.rightView = button
+        textField.rightViewMode = .always
+    }
+    
+    @objc func unhide() {
+        
+        setupSubviews()
+    }
     
     //MARK: - Actions
     
@@ -39,5 +76,18 @@ class SignUpViewController: UIViewController {
     //MARK: - Navigation
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
+
+extension UITextField {
+
+    func underline() {
+        let border = CALayer()
+        let borderWidth = CGFloat(1.0)
+        border.borderColor = UIColor.init(named: "Salmon")?.cgColor
+        border.frame = CGRect(x: 0, y: self.frame.size.height - borderWidth, width: self.frame.size.width, height: self.frame.size.height)
+        border.borderWidth = borderWidth
+        self.layer.addSublayer(border)
+        self.layer.masksToBounds = true
     }
 }
