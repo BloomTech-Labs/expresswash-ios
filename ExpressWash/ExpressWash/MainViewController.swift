d2bb19107bc1736641b5487749f00db4ad5d5f7d
Robert Keffury <bobby.keffury@me.com>
2020-04-21 21:06:17 -0700

    Added MainVCFile & Fixed Up Next Function.

---
+++ ExpressWash/ExpressWash/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  ExpressWash
+//
+//  Created by Bobby Keffury on 4/21/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
