769e5b0ea9be6e2ae3e69753654276ac290ccbe0
Robert Keffury <bobby.keffury@me.com>
2020-04-22 18:42:02 -0700

    Added Storyboard Layout & Wired up VC Files Accordingly

---
+++ ExpressWash/ExpressWash/ProfileViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileViewController.swift
+//  ExpressWash
+//
+//  Created by Bobby Keffury on 4/22/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    //MARK: - Properties
+    
+    //MARK: - Outlets
+    
+    //MARK: - Views
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    //MARK: - Methods
+    
+    //MARK: - Actions
+    
+    //MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+}
