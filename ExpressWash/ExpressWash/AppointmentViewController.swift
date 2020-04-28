d751463f15710103dd850d141d5e10f068b2f6f9
Robert Keffury <bobby.keffury@me.com>
2020-04-27 19:41:14 -0700

    Added AppointmentVCFile & Wired Up.

---
+++ ExpressWash/ExpressWash/AppointmentViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AppointmentViewController.swift
+//  ExpressWash
+//
+//  Created by Bobby Keffury on 4/27/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class AppointmentViewController: UIViewController {
+
+    //MARK: - Properties
+    
+    //MARK: - Outlets
+    
+    //MARK: - Views
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    //MARK: - Methods
+    
+    //MARK: - Actions
+    
+    //MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+}
