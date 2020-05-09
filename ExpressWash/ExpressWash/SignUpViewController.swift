f5a3b5cc11badbfaac9382ccbf2bb83b90be1fcd
Robert Keffury <bobby.keffury@me.com>
2020-05-08 19:30:18 -0700

    Removed comments.

--- ExpressWash/ExpressWash/SignUpViewController.swift
+++ ExpressWash/ExpressWash/SignUpViewController.swift
@@ -59,20 +59,8 @@ class SignUpViewController: UIViewController {
 
     func setupTextFields() {
         firstNameTextField.delegate = self
-//        firstNameTextField.layer.borderColor = UIColor(named: "Light Blue")?.cgColor
-//        firstNameTextField.layer.borderWidth = 2.0
-//        firstNameTextField.layer.cornerRadius = 5.0
-        
         lastNameTextField.delegate = self
-//        lastNameTextField.layer.borderColor = UIColor(named: "Light Blue")?.cgColor
-//        lastNameTextField.layer.borderWidth = 2.0
-//        lastNameTextField.layer.cornerRadius = 5.0
-        
         emailAddressTextField.delegate = self
-//        emailAddressTextField.layer.borderColor = UIColor(named: "Light Blue")?.cgColor
-//        emailAddressTextField.layer.borderWidth = 2.0
-//        emailAddressTextField.layer.cornerRadius = 5.0
-        
         passwordTextField.delegate = self
     }
 
