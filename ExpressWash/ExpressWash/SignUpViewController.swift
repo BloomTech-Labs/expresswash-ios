725f29055074693d0d593c570aab61af5dd7d624
Robert Keffury <bobby.keffury@me.com>
2020-05-11 20:16:29 -0700

    Cleaned up white space

--- ExpressWash/ExpressWash/SignUpViewController.swift
+++ ExpressWash/ExpressWash/SignUpViewController.swift
@@ -113,7 +113,7 @@ class SignUpViewController: UIViewController {
         guard let confirm = confirmTextField.text, !confirm.isEmpty else { return }
 
         if password == confirm {
-            //Register User
+            userController.registerUser(with: firstName, lastName, email, password)
             self.performSegue(withIdentifier: "finishedSignUpSegue", sender: self)
         } else {
             let alertController = UIAlertController(title: "Passwords Don't Match", message: "", preferredStyle: .alert)
