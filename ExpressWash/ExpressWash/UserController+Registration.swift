316f155e5d1862ba8ffa5e59e9cdeb1c36bb8733
Robert Keffury <bobby.keffury@me.com>
2020-05-11 19:56:45 -0700

    Added register extension to userController

---
+++ ExpressWash/ExpressWash/UserController+Registration.swift
@@ -0,0 +1,15 @@
+//
+//  UserController+Registration.swift
+//  ExpressWash
+//
+//  Created by Bobby Keffury on 5/11/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import Foundation
+
+extension UserController {
+    func registerUser(with firstName: String, lastName: String, emailAddress: String, password: String, completion: (Error?) -> Void) {
+        
+    }
+}
