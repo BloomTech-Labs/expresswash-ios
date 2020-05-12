dd9141773f74f3bd98a5e618e7027c111f9e7bc0
Robert Keffury <bobby.keffury@me.com>
2020-05-11 20:39:53 -0700

    Added register user function

--- ExpressWash/ExpressWash/UserController+Registration.swift
+++ ExpressWash/ExpressWash/UserController+Registration.swift
@@ -9,7 +9,36 @@
 import Foundation
 
 extension UserController {
-    func registerUser(with firstName: String, _ lastName: String, _ emailAddress: String, _ password: String) {
 
+    typealias CompletionHandler = (Error?) -> Void
+
+    func registerUser(with firstName: String, _ lastName: String, _ emailAddress: String, _ password: String, completion: @escaping CompletionHandler = { _ in }) {
+
+        let register = ENDPOINTS.registerClient
+        let registerUrl = BASEURL.appendingPathComponent(register.rawValue)
+
+        var request = URLRequest(url: registerUrl)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        do {
+            let userParams = ["firstName": firstName, "lastName": lastName, "emailAddress": emailAddress, "password": password] as [String: Any]
+            let json = try JSONSerialization.data(withJSONObject: userParams, options: .prettyPrinted)
+            request.httpBody = json
+        } catch {
+            print("Error encoding user object: \(error)")
+        }
+
+        URLSession.shared.dataTask(with: request) { ( _, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+
+            if let error = error { completion(error); return}
+
+            completion(nil)
+        }.resume()
     }
 }
