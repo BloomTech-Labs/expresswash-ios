9dee7dfb6912a8837c7c15e13617e0cb91d062ee
Robert Keffury <bobby.keffury@me.com>
2020-05-12 20:08:50 -0700

    Added receipt tableViewCell file & wired up

---
+++ ExpressWash/ExpressWash/ReceiptTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ReceiptTableViewCell.swift
+//  ExpressWash
+//
+//  Created by Bobby Keffury on 5/12/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class ReceiptTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var washerImage: UIImageView!
+    @IBOutlet weak var washerName: UILabel!
+    @IBOutlet weak var washerRating: UILabel!
+    @IBOutlet weak var timeTakenLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var beforeImageView: UIImageView!
+    @IBOutlet weak var afterImageView: UIImageView!
+}
