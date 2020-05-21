32034059806d39cc8c3920a8ed83c740235f02c4
Robert Keffury <bobby.keffury@me.com>
2020-05-20 17:12:54 -0700

    Added JobController file and installed Podfile

---
+++ ExpressWash/ExpressWash/JobController.swift
@@ -0,0 +1,21 @@
+//
+//  JobController.swift
+//  ExpressWash
+//
+//  Created by Bobby Keffury on 5/20/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class JobController {
+    // MARK: - Properties
+    
+    typealias CompletionHandler = (Job?, Error?) -> Void
+    
+    // MARK: - Local Methods
+    
+    // MARK: - Network Methods
+    
+}
