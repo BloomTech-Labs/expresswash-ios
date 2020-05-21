ac0b9482165e55190991127cc3cc43c02b1c224e
Robert Keffury <bobby.keffury@me.com>
2020-05-21 09:04:21 -0700

    Tests are up and running.

--- ExpressWash/ExpressWash/Job+CoreDataProperties.swift
+++ ExpressWash/ExpressWash/Job+CoreDataProperties.swift
@@ -17,27 +17,28 @@ extension Job {
     }
 
     // Attributes
+    @NSManaged public var washAddress: String
     @NSManaged public var address: String
     @NSManaged public var address2: String?
     @NSManaged public var city: String
     @NSManaged public var completed: Bool
     @NSManaged public var jobId: Int32
-    @NSManaged public var jobLocationLat: Double
-    @NSManaged public var jobLocationLon: Double
+    @NSManaged public var jobLocationLat: Float
+    @NSManaged public var jobLocationLon: Float
     @NSManaged public var jobType: String
     @NSManaged public var notes: String?
     @NSManaged public var paid: Bool
-    @NSManaged public var photoBeforeJob: URL?
-    @NSManaged public var photoAfterJob: URL?
+    @NSManaged public var photoBeforeJob: String?
+    @NSManaged public var photoAfterJob: String?
     @NSManaged public var scheduled: Bool
     @NSManaged public var state: String
-    @NSManaged public var timeCompleted: Date?
-    @NSManaged public var timeRequested: Date
+    @NSManaged public var timeCompleted: String?
+    @NSManaged public var timeRequested: String
     @NSManaged public var zip: String
 
     // Relationships
-    @NSManaged public var car: Car
-    @NSManaged public var client: User
-    @NSManaged public var washer: Washer
+    @NSManaged public var car: Car?
+    @NSManaged public var client: User?
+    @NSManaged public var washer: Washer?
 
 }
