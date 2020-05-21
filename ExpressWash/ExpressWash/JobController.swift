5e268e6784bc52343d6208ef9d7f94336eb159f5
Robert Keffury <bobby.keffury@me.com>
2020-05-20 17:37:05 -0700

    Added outlines for necessary functions

--- ExpressWash/ExpressWash/JobController.swift
+++ ExpressWash/ExpressWash/JobController.swift
@@ -10,12 +10,44 @@ import Foundation
 import CoreData
 
 class JobController {
+    
     // MARK: - Properties
     
     typealias CompletionHandler = (Job?, Error?) -> Void
     
     // MARK: - Local Methods
     
+    func addJob(jobRepresentation: JobRepresentation,
+                context: NSManagedObjectContext = CoreDataStack.shared.mainContext,
+                completion: @escaping CompletionHandler) {
+    }
+    
+    func updateJob(jobRepresentation: JobRepresentation,
+                   context: NSManagedObjectContext = CoreDataStack.shared.mainContext,
+                   completion: @escaping CompletionHandler) {
+    }
+    
+    func deleteJob(jobRepresentation: JobRepresentation,
+                   context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+    }
+    
     // MARK: - Network Methods
     
+    func createJob(jobRepresentation: JobRepresentation, completion: @escaping CompletionHandler) {
+    }
+    
+    func getJobInfo(jobRepresentation: JobRepresentation, completion: @escaping CompletionHandler) {
+    }
+    
+    func getUserJobs(user: User, completion: @escaping ([Job?], Error?) -> Void) {
+    }
+    
+    func assignWasher(jobRepresentation: JobRepresentation, washerID: Int, completion: @escaping CompletionHandler) {
+    }
+    
+    func editJob(jobRepresentation: JobRepresentation, completion: @escaping CompletionHandler) {
+    }
+    
+    func deleteJob(jobRepresentation: JobRepresentation, completion: @escaping (String?, Error?) -> Void) {
+    }
 }
