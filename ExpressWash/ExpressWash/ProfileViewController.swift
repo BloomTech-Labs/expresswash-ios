3979e02310134bc440bd05fab973287196d4ff47
Robert Keffury <49419310+Keffury1@users.noreply.github.com>
2020-05-26 19:37:18 -0700

    Profile Page Layout (#9)
    * Added update views method and collection view dataSource methods
    
    * Created edit profile page.
    
    * Wired up EditProfileVC & connected segues
    
    * Added addCarsVC & wired up
    
    * Added flow and tapGesture for editVC
    
    * Fixed swifLint error
    
    * Removed edit screens.
    
    * Added appropriate storyboard features.

--- ExpressWash/ExpressWash/ProfileViewController.swift
+++ ExpressWash/ExpressWash/ProfileViewController.swift
@@ -8,21 +8,22 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
-
-    // MARK: - Properties
+class ProfileViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     // MARK: - Outlets
 
+    @IBOutlet var profileTapGesture: UITapGestureRecognizer!
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var ratingLabel: UILabel!
     @IBOutlet weak var editButton: UIButton!
     @IBOutlet weak var bannerImageView: UIImageView!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var phoneNumberLabel: UILabel!
-    @IBOutlet weak var emailAddressLabel: UILabel!
-    @IBOutlet weak var addressLabel: UILabel!
-    @IBOutlet weak var cityStateZipLabel: UILabel!
+    @IBOutlet weak var bannerImageButton: UIButton!
+    @IBOutlet weak var fullNameTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var emailAddressTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var cityStateZipTextField: UITextField!
+    @IBOutlet weak var addCarsButton: UIButton!
     @IBOutlet weak var carsCollectionView: UICollectionView!
 
     // MARK: - Views
@@ -31,6 +32,35 @@ class ProfileViewController: UIViewController {
         super.viewDidLoad()
 
         setupSubviews()
+        updateViews()
+        carsCollectionView.delegate = self
+        carsCollectionView.dataSource = self
+    }
+
+    // MARK: - CollectionView Data Source
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        guard let user = UserController.shared.sessionUser, let cars = user.cars else { return 0 }
+
+        return cars.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "carCell", for: indexPath)
+            as? CarCollectionViewCell,
+            let user = UserController.shared.sessionUser,
+            let cars = user.cars else { return UICollectionViewCell() }
+
+        if let car = cars[indexPath.row] as? Car {
+            if let photo = car.photo {
+                cell.imageView.image = UIImage(contentsOfFile: photo)
+            }
+        }
+
+        cell.layer.cornerRadius = 10.0
+
+        return cell
     }
 
     // MARK: - Methods
@@ -41,14 +71,43 @@ class ProfileViewController: UIViewController {
         profileImageView.layer.borderWidth = 3.0
     }
 
+    func updateViews() {
+        guard let user = UserController.shared.sessionUser else { return }
+
+        if let url = user.profilePicture {
+            if let data = try? Data(contentsOf: url) {
+                       let image: UIImage = UIImage(data: data)!
+                       profileImageView.image = image
+                   }
+        }
+
+        ratingLabel.text = "★ \(user.userRating)"
+
+        if let bannerURL = user.bannerImage {
+            if let data = try? Data(contentsOf: bannerURL) {
+                let image = UIImage(data: data)
+                bannerImageView.image = image
+            }
+        }
+
+        fullNameTextField.text = "\(user.firstName.capitalized) \(user.lastName.capitalized)"
+        phoneNumberTextField.text = user.phoneNumber
+        emailAddressTextField.text = user.email
+        addressTextField.text = user.streetAddress
+        cityStateZipTextField.text = "\(user.city ?? "city"), \(user.state ?? "state"), \(user.zip ?? "zip")"
+    }
+
     // MARK: - Actions
 
     @IBAction func editButtonTapped(_ sender: Any) {
     }
 
-    // MARK: - Navigation
+    @IBAction func profileImageTapped(_ sender: Any) {
+    }
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    @IBAction func addCarsButtonTapped(_ sender: Any) {
+    }
 
+    @IBAction func addBannerImageButtonTapped(_ sender: Any) {
     }
 }
