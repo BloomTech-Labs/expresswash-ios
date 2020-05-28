f8072a8af6dba3f5b420ac2dd8a617440b60ce4c
Joel <joelgroomer@gmail.com>
2020-05-27 21:21:59 -0700

    Helper file to make converting date strings to clock times easier.

---
+++ ExpressWash/ExpressWash/DateFormatter+Clock.swift
@@ -0,0 +1,32 @@
+//
+//  DateFormatter+Clock.swift
+//  ExpressWash
+//
+//  Created by Joel Groomer on 5/27/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import Foundation
+
+extension DateFormatter {
+    static var Clock: DateFormatter = {
+       let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        return formatter
+    }()
+    
+    static var FromISODate: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        return formatter
+    }()
+    
+    static func clockString(from timeString: String) -> String {
+        if let date = DateFormatter.FromISODate.date(from: timeString) {
+            return DateFormatter.Clock.string(from: date)
+        }
+
+        return "00:00?"
+    }
+}
