d4616fbdf48cdbbbb0d684e75c7552d65bfdaf1a
Robert Keffury <bobby.keffury@me.com>
2020-05-29 19:58:33 -0700

    Commit to rebase. Added carViewController

---
+++ ExpressWash/ExpressWash/AddCarViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AddCarViewController.swift
+//  ExpressWash
+//
+//  Created by Bobby Keffury on 5/29/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class AddCarViewController: UIViewController {
+
+    // MARK: - Properties
+
+    // MARK: - Outlets
+
+    // MARK: - Views
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Methods
+
+    // MARK: - Actions
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    }
+}
