27a50b230a763f05493e63cd8044d881b8ce7b9c
Joel <joelgroomer@gmail.com>
2020-05-29 19:04:41 -0700

    Extended UIImage to maintain a cache to avoid having to download user and car images multiple times in one session.

---
+++ ExpressWash/ExpressWash/UIImage+cached.swift
@@ -0,0 +1,51 @@
+//
+//  UIImage+cached.swift
+//  ExpressWash
+//
+//  Created by Joel Groomer on 5/29/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    static var cache: [String: UIImage] = [:]
+
+    static func cached(from urlString: String, defaultTitle: String? = nil) -> UIImage? {
+        var returnImage: UIImage?
+
+        if !UIImage.cache.keys.contains(urlString) ||
+            UIImage.cache[urlString] == nil {
+            if let url = URL(string: urlString) {
+                if let data = try? Data(contentsOf: url) {
+                    returnImage = UIImage(data: data)
+                    UIImage.cache[urlString] = returnImage
+                    print("Downloaded and cached image for \(urlString)")
+                } else {
+                    returnImage = UIImage(named: defaultTitle ?? "")
+                }
+            } else {
+                returnImage = UIImage(named: defaultTitle ?? "")
+            }
+        } else {
+            returnImage = UIImage.cache[urlString]
+            print("Using cached image for \(urlString)")
+        }
+
+        return returnImage
+    }
+
+    static func cached(from url: URL, defaultTitle: String? = nil) -> UIImage? {
+        cached(from: url.absoluteString, defaultTitle: defaultTitle)
+    }
+
+    static func refreshCache(for urlString: String) {
+        UIImage.cache[urlString] = nil  // clear out the stored image
+        _ = cached(from: urlString)     // request it again from server
+    }
+
+    static func refreshCache(for url: URL) {
+        refreshCache(for: url.absoluteString)
+    }
+}
