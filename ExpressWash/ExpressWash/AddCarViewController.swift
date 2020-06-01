350caedc1dd4e3b0e5b5b297d0f51be7b716d360
Robert Keffury <bobby.keffury@me.com>
2020-06-01 12:33:16 -0700

    Adding cars is functional
    Waiting to implement photos until backend is ready.

--- ExpressWash/ExpressWash/AddCarViewController.swift
+++ ExpressWash/ExpressWash/AddCarViewController.swift
@@ -12,33 +12,90 @@ class AddCarViewController: UIViewController {
 
     // MARK: - Properties
 
+    private var carController = CarController()
+    var user: User?
+
+    // MARK: - Outlets
+
     @IBOutlet weak var carImageView: UIImageView!
+    @IBOutlet weak var captureImageButton: UIButton!
     @IBOutlet weak var yearTextField: UITextField!
     @IBOutlet weak var makeTextField: UITextField!
     @IBOutlet weak var modelTextField: UITextField!
     @IBOutlet weak var licenseTextField: UITextField!
     @IBOutlet weak var colorTextField: UITextField!
-    @IBOutlet weak var categorySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var categoryTextField: UITextField!
+    @IBOutlet weak var sizeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var addCarButton: UIButton!
-
-    // MARK: - Outlets
 
     // MARK: - Views
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupSubviews()
     }
 
     // MARK: - Methods
 
+    private func setupSubviews() {
+        let titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        UISegmentedControl.appearance().setTitleTextAttributes(titleTextAttributes, for: .normal)
+
+        captureImageButton.isSelected = false
+        addCarButton.layer.cornerRadius = 10.0
+        setupCamera()
+    }
+
+    private func setupCamera() {
+        //Set this up when backend allows it.
+    }
+
     // MARK: - Actions
 
     @IBAction func addCarButtonTapped(_ sender: Any) {
+        guard let year = yearTextField.text else { return }
+        guard let make = makeTextField.text else { return }
+        guard let model = modelTextField.text else { return }
+        guard let licensePlate = licenseTextField.text else { return }
+        guard let color = colorTextField.text else { return }
+        guard let category = categoryTextField.text else { return }
+        let segment = sizeSegmentedControl.selectedSegmentIndex
+        guard let size = sizeSegmentedControl.titleForSegment(at: segment) else { return }
+
+        guard let yearInt = Int16(year) else { return }
+
+        let carRepresentation = CarRepresentation(carId: nil,
+                                                  make: make,
+                                                  model: model,
+                                                  year: yearInt,
+                                                  color: color,
+                                                  licensePlate: licensePlate,
+                                                  photo: "HANDLE THIS",
+                                                  category: category,
+                                                  size: size)
+
+        carController.addCar(carRepresentation: carRepresentation) { (car, error) in
+            if let error = error {
+                print("Error creating car: \(error)")
+                return
+            }
+
+            guard let car = car else { return }
+
+            self.user?.addToCars(car)
+
+            self.dismiss(animated: true, completion: nil)
+        }
     }
 
-    // MARK: - Navigation
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-
+    @IBAction func captureImageButtonTapped(_ sender: Any) {
+        if captureImageButton.isSelected {
+            captureImageButton.isSelected = false
+            //Show Camera
+        } else {
+            //Take Picture & Set it
+            captureImageButton.isSelected = true
+        }
     }
 }
--- ExpressWash/ExpressWash/ProfileViewController.swift
+++ ExpressWash/ExpressWash/ProfileViewController.swift
@@ -125,108 +125,6 @@ class ProfileViewController: UIViewController,
         editDisabled()
     }
 
-    func editEnabled() {
-        editButton.setBackgroundImage(UIImage(systemName: "square.and.arrow.down"), for: .normal)
-        editButton.isSelected = true
-
-        if !profileTapGesture.isEnabled {
-            profileImageView.image = UIImage(systemName: "plus.circle")
-        }
-
-        if firstNameTextField.text == "First" {
-            firstNameTextField.text = nil
-        }
-
-        if lastNameTextField.text == "Last" {
-            lastNameTextField.text = nil
-        }
-
-        if phoneNumberTextField.text == "Phone Number" {
-            phoneNumberTextField.text = nil
-        }
-
-        if emailAddressTextField.text == "Email Address" {
-            emailAddressTextField.text = nil
-        }
-
-        if addressTextField.text == "Address" {
-            addressTextField.text = nil
-        }
-
-        if cityTextField.text == "City" {
-            cityTextField.text = nil
-        }
-
-        if stateTextField.text == "State" {
-            stateTextField.text = nil
-        }
-
-        bannerImageButton.alpha = 1
-        bannerImageButton.isEnabled = true
-        profileTapGesture.isEnabled = true
-        firstNameTextField.isEnabled = true
-        lastNameTextField.isEnabled = true
-        phoneNumberTextField.isEnabled = true
-        emailAddressTextField.isEnabled = true
-        addressTextField.isEnabled = true
-        cityTextField.isEnabled = true
-        stateTextField.isEnabled = true
-        carsCollectionView.alpha = 0
-        addCarsButton.alpha = 1
-        addCarsButton.isEnabled = true
-    }
-
-    func editDisabled() {
-        editButton.setBackgroundImage(UIImage(systemName: "pencil"), for: .normal)
-        editButton.isSelected = false
-
-        if profileTapGesture.isEnabled {
-            profileImageView.image = UIImage(systemName: "person.circle")
-        }
-
-        if firstNameTextField.text == "" {
-            firstNameTextField.text = "First"
-        }
-
-        if lastNameTextField.text == "" {
-            lastNameTextField.text = "Last"
-        }
-
-        if phoneNumberTextField.text == "" {
-            phoneNumberTextField.text = "Phone Number"
-        }
-
-        if emailAddressTextField.text == "" {
-            emailAddressTextField.text = "Email Address"
-        }
-
-        if addressTextField.text == "" {
-            addressTextField.text = "Address"
-        }
-
-        if cityTextField.text == "" {
-            cityTextField.text = "City"
-        }
-
-        if stateTextField.text == "" {
-            stateTextField.text = "State"
-        }
-
-        bannerImageButton.alpha = 0
-        bannerImageButton.isEnabled = false
-        profileTapGesture.isEnabled = false
-        firstNameTextField.isEnabled = false
-        lastNameTextField.isEnabled = false
-        phoneNumberTextField.isEnabled = false
-        emailAddressTextField.isEnabled = false
-        addressTextField.isEnabled = false
-        cityTextField.isEnabled = false
-        stateTextField.isEnabled = false
-        addCarsButton.alpha = 0
-        addCarsButton.isEnabled = false
-        carsCollectionView.alpha = 1
-    }
-
     func imagePickerController(picker: UIImagePickerController!,
                                didFinishPickingImage image: UIImage!,
                                editingInfo: NSDictionary!) {
@@ -330,5 +228,120 @@ class ProfileViewController: UIViewController,
                 }
             })
         }
+    }
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "addCarSegue" {
+            if let addCarVC = segue.destination as? AddCarViewController {
+                addCarVC.user = UserController.shared.sessionUser
+            }
+        }
+    }
+}
+
+extension ProfileViewController {
+
+    func editEnabled() {
+        editButton.setBackgroundImage(UIImage(systemName: "square.and.arrow.down"), for: .normal)
+        editButton.isSelected = true
+
+        if !profileTapGesture.isEnabled {
+            profileImageView.image = UIImage(systemName: "plus.circle")
+        }
+
+        if firstNameTextField.text == "First" {
+            firstNameTextField.text = nil
+        }
+
+        if lastNameTextField.text == "Last" {
+            lastNameTextField.text = nil
+        }
+
+        if phoneNumberTextField.text == "Phone Number" {
+            phoneNumberTextField.text = nil
+        }
+
+        if emailAddressTextField.text == "Email Address" {
+            emailAddressTextField.text = nil
+        }
+
+        if addressTextField.text == "Address" {
+            addressTextField.text = nil
+        }
+
+        if cityTextField.text == "City" {
+            cityTextField.text = nil
+        }
+
+        if stateTextField.text == "State" {
+            stateTextField.text = nil
+        }
+
+        bannerImageButton.alpha = 1
+        bannerImageButton.isEnabled = true
+        profileTapGesture.isEnabled = true
+        firstNameTextField.isEnabled = true
+        lastNameTextField.isEnabled = true
+        phoneNumberTextField.isEnabled = true
+        emailAddressTextField.isEnabled = true
+        addressTextField.isEnabled = true
+        cityTextField.isEnabled = true
+        stateTextField.isEnabled = true
+        carsCollectionView.alpha = 0
+        addCarsButton.alpha = 1
+        addCarsButton.isEnabled = true
+    }
+
+    func editDisabled() {
+        editButton.setBackgroundImage(UIImage(systemName: "pencil"), for: .normal)
+        editButton.isSelected = false
+
+        if profileTapGesture.isEnabled {
+            profileImageView.image = UIImage(systemName: "person.circle")
+        }
+
+        if firstNameTextField.text == "" {
+            firstNameTextField.text = "First"
+        }
+
+        if lastNameTextField.text == "" {
+            lastNameTextField.text = "Last"
+        }
+
+        if phoneNumberTextField.text == "" {
+            phoneNumberTextField.text = "Phone Number"
+        }
+
+        if emailAddressTextField.text == "" {
+            emailAddressTextField.text = "Email Address"
+        }
+
+        if addressTextField.text == "" {
+            addressTextField.text = "Address"
+        }
+
+        if cityTextField.text == "" {
+            cityTextField.text = "City"
+        }
+
+        if stateTextField.text == "" {
+            stateTextField.text = "State"
+        }
+
+        bannerImageButton.alpha = 0
+        bannerImageButton.isEnabled = false
+        profileTapGesture.isEnabled = false
+        firstNameTextField.isEnabled = false
+        lastNameTextField.isEnabled = false
+        phoneNumberTextField.isEnabled = false
+        emailAddressTextField.isEnabled = false
+        addressTextField.isEnabled = false
+        cityTextField.isEnabled = false
+        stateTextField.isEnabled = false
+        addCarsButton.alpha = 0
+        addCarsButton.isEnabled = false
+        carsCollectionView.alpha = 1
     }
 }
