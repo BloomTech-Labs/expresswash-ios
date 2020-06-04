860ce0ba66d41c8c5381cad2dbf1bdfe2175cdc2
Robert Keffury <bobby.keffury@me.com>
2020-06-03 19:26:39 -0700

    Added tableView functionality, detail page, & fetch jobs function.

---
+++ ExpressWash/ExpressWash/ReceiptDetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ReceiptDetailViewController.swift
+//  ExpressWash
+//
+//  Created by Bobby Keffury on 6/3/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class ReceiptDetailViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    var job: Job?
+
+    // MARK: - Outlets
+
+    // MARK: - Views
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Methods
+
+    // MARK: - Actions
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
