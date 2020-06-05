6574e187344a2ddfd4b6c58674b259b86f7b4313
Robert Keffury <49419310+Keffury1@users.noreply.github.com>
2020-06-04 20:50:45 -0700

    Receipts Page Implemented (#14)
    * Added tableView functionality, detail page, & fetch jobs function.
    
    * Added skeleton layout to receiptDetailVC
    
    Also updated build number to get app on connect.
    
    * Added base layout for receiptDetailVC
    
    * Changed layout of storyboard.
    
    * Added rating ability.
    
    * Added updateViews method to detailVC

--- ExpressWash/ExpressWash/ReceiptsViewController.swift
+++ ExpressWash/ExpressWash/ReceiptsViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
-class ReceiptsViewController: UIViewController {
+class ReceiptsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     // MARK: - Properties
+
+    let jobController = JobController()
+    var jobs: [Job] = []
 
     // MARK: - Outlets
 
@@ -20,15 +23,74 @@ class ReceiptsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        getJobs()
     }
 
     // MARK: - Methods
 
-    // MARK: - Actions
+    private func getJobs() {
+        guard let user = UserController.shared.sessionUser else { return }
+
+        jobController.getUserJobs(user: user) { (jobRepresentations, error) in
+            if let error = error {
+                print("Error getting user's jobs: \(error)")
+                return
+            }
+
+            guard let jobReps = jobRepresentations else { return }
+
+            self.jobs = []
+
+            for rep in jobReps {
+                let job = Job(representation: rep)
+                self.jobs.append(job)
+            }
+
+            self.receiptsTableView.reloadData()
+        }
+    }
+
+    // MARK: - Table View
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return jobs.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "receiptCell",
+                                                       for: indexPath) as? ReceiptTableViewCell else {
+                                                        return UITableViewCell() }
+
+        let job = jobs[indexPath.row]
+
+        let firstName = job.washer!.user!.firstName
+        let lastName = job.washer!.user!.lastName
+
+        cell.washerName.text = firstName + lastName
+
+        // Handle washer profile photo
+
+        cell.washerRating.text = "★ \(job.washer!.washerRating))"
+
+        // Ask for backend to add date requested to job
+
+        // Also add timeArrived to help calculate timeTaken
+
+        // Handle before & after pictures
+
+        return cell
+    }
 
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-
+        if segue.identifier == "FILL ME IN" {
+            if let receiptDetailVC = segue.destination as? ReceiptDetailViewController,
+                let indexPath = receiptsTableView.indexPathForSelectedRow {
+                let job = jobs[indexPath.row]
+                receiptDetailVC.job = job
+            }
+        }
     }
 }
