e1dce619bac9e995017d02e5aaa2ba7e3e88f42f
Robert Keffury <bobby.keffury@me.com>
2020-06-17 21:11:30 -0700

    Added necessary features for purchasing wash.

--- ExpressWash/ExpressWash/PaymentViewController.swift
+++ ExpressWash/ExpressWash/PaymentViewController.swift
@@ -9,20 +9,29 @@
 import UIKit
 import Stripe
 
-class PaymentViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource,
-STPPaymentOptionsViewControllerDelegate, STPAddCardViewControllerDelegate {
+class PaymentViewController: UIViewController, UICollectionViewDelegate,
+UICollectionViewDataSource, STPAuthenticationContext {
 
     // MARK: - Properties
 
-    let customerContext = MockCustomerContext()
+    lazy var cardTextField: STPPaymentCardTextField = {
+        let cardTextField = STPPaymentCardTextField()
+        return cardTextField
+    }()
+    var jobRep: JobRepresentation?
+    var cars: [Car] {
+        let orderedSet = UserController.shared.sessionUser.user?.cars?.set as? Set<Car> ?? []
+        return orderedSet.sorted { (car1, car2) -> Bool in
+            car1.carId > car2.carId
+        }
+    }
+    var amount: Int?
 
     // MARK: - Outlets
 
     @IBOutlet weak var carsCollectionView: UICollectionView!
-    @IBOutlet weak var selectPaymentView: UIView!
-    @IBOutlet var doubleTapGesture: UITapGestureRecognizer!
+    @IBOutlet weak var cardView: UIView!
     @IBOutlet weak var confirmWashButton: UIButton!
-    @IBOutlet weak var selectPaymentLabel: UILabel!
 
     // MARK: - Views
 
@@ -30,67 +39,78 @@ STPPaymentOptionsViewControllerDelegate, STPAddCardViewControllerDelegate {
         super.viewDidLoad()
 
         confirmWashButton.layer.cornerRadius = 10.0
-        selectPaymentView.layer.cornerRadius = 10.0
-    }
-
-    // MARK: - Payment
-
-    func paymentOptionsViewController(_ paymentOptionsViewController: STPPaymentOptionsViewController,
-                                      didFailToLoadWithError error: Error) {
-        dismiss(animated: true, completion: nil)
-    }
-
-    func paymentOptionsViewControllerDidFinish(_ paymentOptionsViewController: STPPaymentOptionsViewController) {
-        dismiss(animated: true, completion: nil)
-    }
-
-    func paymentOptionsViewControllerDidCancel(_ paymentOptionsViewController: STPPaymentOptionsViewController) {
-        dismiss(animated: true, completion: nil)
-    }
-
-    func addCardViewControllerDidCancel(_ addCardViewController: STPAddCardViewController) {
-        dismiss(animated: true, completion: nil)
-    }
-
-    func addCardViewController(_ addCardViewController: STPAddCardViewController,
-                               didCreatePaymentMethod paymentMethod: STPPaymentMethod,
-                               completion: @escaping STPErrorBlock) {
-        dismiss(animated: true, completion: nil)
+        cardView.addSubview(cardTextField)
+        cardTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            cardTextField.leadingAnchor.constraint(equalTo: cardView.leadingAnchor),
+            cardTextField.trailingAnchor.constraint(equalTo: cardView.trailingAnchor),
+            cardTextField.centerYAnchor.constraint(equalTo: cardView.centerYAnchor),
+            cardTextField.centerXAnchor.constraint(equalTo: cardView.centerXAnchor),
+            cardTextField.heightAnchor.constraint(equalToConstant: 50.0)
+        ])
+        if let amount = amount {
+            StripeController.shared.startCheckout(with: amount)
+        }
     }
 
     // MARK: - CollectionView
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return cars.count
     }
 
     func collectionView(_ collectionView: UICollectionView,
                         cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "carCell",
+                                                            for: indexPath) as? CarCollectionViewCell else {
+                                                                return UICollectionViewCell() }
+
+        let car = cars[indexPath.row]
+
+        if let photoString = car.photo {
+            cell.imageView.image = UIImage.cached(from: photoString, defaultTitle: nil)
+        }
+
+        return cell
     }
 
     // MARK: - Methods
 
     // MARK: - Actions
 
-    @IBAction func doubleTapGestureComplete(_ sender: Any) {
-        let theme = STPTheme()
+    @IBAction func confirmWashButtonTapped(_ sender: Any) {
+        guard let paymentIntentClientSecret = StripeController.shared.paymentIntentClientSecret else { return }
+        // Collect card details
+        let cardParams = cardTextField.cardParams
+        let paymentMethodParams = STPPaymentMethodParams(card: cardParams, billingDetails: nil, metadata: nil)
+        let paymentIntentParams = STPPaymentIntentParams(clientSecret: paymentIntentClientSecret)
+        paymentIntentParams.paymentMethodParams = paymentMethodParams
 
-        let config = STPPaymentConfiguration()
-        config.additionalPaymentOptions = .default
-        config.requiredBillingAddressFields = .none
-        config.appleMerchantIdentifier = "dummy-merchant-id"
-        let viewController = STPPaymentOptionsViewController(configuration: config,
-                                                             theme: theme,
-                                                             customerContext: self.customerContext,
-                                                             delegate: self)
-        viewController.apiClient = MyAPIClient()
-        let navigationController = UINavigationController(rootViewController: viewController)
-        navigationController.navigationBar.stp_theme = theme
-        present(navigationController, animated: true, completion: nil)
+        // Submit the payment
+        let paymentHandler = STPPaymentHandler.shared()
+        paymentHandler.confirmPayment(withParams: paymentIntentParams,
+                                      authenticationContext: self) { (status, _, _) in
+            switch status {
+            case .failed:
+                // Alert of failed payment
+                print("Payment Failed")
+                break
+            case .canceled:
+                // Alert of cenceled payment
+                print("Payment Canceled")
+                break
+            case .succeeded:
+                // Alert of succeeded payment
+                print("Payment Successful")
+                break
+            @unknown default:
+                fatalError()
+            }
+        }
     }
 
-    @IBAction func confirmWashButtonTapped(_ sender: Any) {
+    func authenticationPresentingViewController() -> UIViewController {
+        return self
     }
 
     // MARK: - Navigation
--- ExpressWash/ExpressWash/StripeController.swift
+++ ExpressWash/ExpressWash/StripeController.swift
@@ -9,107 +9,37 @@
 import Foundation
 import Stripe
 
-class MyAPIClient: STPAPIClient {
-    override func createPaymentMethod(with paymentMethodParams: STPPaymentMethodParams,
-                                      completion: @escaping STPPaymentMethodCompletionBlock) {
-        guard let card = paymentMethodParams.card,
-            let billingDetails = paymentMethodParams.billingDetails else { return }
+class StripeController {
 
-        var cardJSON: [String: Any] = [:]
-        var billingDetailsJSON: [String: Any] = [:]
-        cardJSON["id"] = "\(card.hashValue)"
-        cardJSON["exp_month"] = "\(card.expMonth ?? 0)"
-        cardJSON["exp_year"] = "\(card.expYear ?? 0)"
-        cardJSON["last4"] = card.number?.suffix(4)
-        billingDetailsJSON["name"] = billingDetails.name
-        billingDetailsJSON["line1"] = billingDetails.address?.line1
-        billingDetailsJSON["line2"] = billingDetails.address?.line2
-        billingDetailsJSON["state"] = billingDetails.address?.state
-        billingDetailsJSON["postal_code"] = billingDetails.address?.postalCode
-        billingDetailsJSON["country"] = billingDetails.address?.country
-        cardJSON["country"] = billingDetails.address?.country
-        if let number = card.number {
-            let brand = STPCardValidator.brand(forNumber: number)
-            cardJSON["brand"] = STPCard.string(from: brand)
-        }
-        cardJSON["fingerprint"] = "\(card.hashValue)"
-        cardJSON["country"] = "US"
-        let paymentMethodJSON: [String: Any] = [
-            "id": "\(card.hashValue)",
-            "object": "payment_method",
-            "type": "card",
-            "livemode": false,
-            "created": NSDate().timeIntervalSince1970,
-            "used": false,
-            "card": cardJSON,
-            "billing_details": billingDetailsJSON
+    static var shared = StripeController()
+
+    var paymentIntentClientSecret: String?
+
+    func startCheckout(with amount: Int) {
+        // Create a PaymentIntent by calling the sample server's /create-payment-intent endpoint.
+        let url = BASEURL.appendingPathComponent("create-payment-intent")
+        let json: [String: Any] = [
+            "amount": amount,
+            "currency": "usd"
         ]
-        let paymentMethod = STPPaymentMethod.decodedObject(fromAPIResponse: paymentMethodJSON)
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
-            completion(paymentMethod, nil)
-        }
-    }
-}
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONSerialization.data(withJSONObject: json)
+        let task = URLSession.shared.dataTask(with: request, completionHandler: { [weak self] (data, response, error) in
+            guard let response = response as? HTTPURLResponse,
+                response.statusCode == 200,
+                let data = data,
+                let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                let clientSecret = json["clientSecret"] as? String else { return }
 
-class MockCustomer: STPCustomer {
-    var paymentMethods: [STPPaymentMethod] = []
-    var defaultPaymentMethod: STPPaymentMethod?
-
-    var paymentMethodArray: [STPPaymentMethod] {
-        get {
-            return paymentMethods
-        }
-        set {
-            paymentMethods = newValue
-        }
-    }
-
-    var defaultPaymentMethods: STPPaymentMethod? {
-        get {
-            return defaultPaymentMethod
-        }
-        set {
-            defaultPaymentMethod = newValue
-        }
-    }
-}
-
-class MockCustomerContext: STPCustomerContext {
-
-    let customer = MockCustomer()
-
-    override func retrieveCustomer(_ completion: STPCustomerCompletionBlock? = nil) {
-        if let completion = completion {
-            completion(customer, nil)
-        }
-    }
-
-    override func attachPaymentMethod(toCustomer paymentMethod: STPPaymentMethod, completion: STPErrorBlock? = nil) {
-        customer.paymentMethods.append(paymentMethod)
-        if let completion = completion {
-            completion(nil)
-        }
-    }
-
-    override func detachPaymentMethod(fromCustomer paymentMethod: STPPaymentMethod, completion: STPErrorBlock? = nil) {
-        if let index = customer.paymentMethods.firstIndex(where: { $0.stripeId == paymentMethod.stripeId }) {
-            customer.paymentMethods.remove(at: index)
-        }
-        if let completion = completion {
-            completion(nil)
-        }
-    }
-
-    override func listPaymentMethodsForCustomer(completion: STPPaymentMethodsCompletionBlock? = nil) {
-        if let completion = completion {
-            completion(customer.paymentMethods, nil)
-        }
-    }
-
-    func selectDefaultCustomerPaymentMethod(_ paymentMethod: STPPaymentMethod, completion: @escaping STPErrorBlock) {
-        if customer.paymentMethods.contains(where: { $0.stripeId == paymentMethod.stripeId }) {
-            customer.defaultPaymentMethod = paymentMethod
-        }
-        completion(nil)
+            if let error = error {
+                print("Error creating payment intent: \(error)")
+                return
+            }
+            print("Created PaymentIntent")
+            self?.paymentIntentClientSecret = clientSecret
+        })
+        task.resume()
     }
 }
