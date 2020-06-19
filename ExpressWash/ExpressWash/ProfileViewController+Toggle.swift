9276f590ae2bc32182742c029ccd599ce25fa69d
Robert Keffury <bobby.keffury@me.com>
2020-06-19 13:41:10 -0700

    Adding cars from payment page implemented.

--- ExpressWash/ExpressWash/ProfileViewController+Toggle.swift
+++ ExpressWash/ExpressWash/ProfileViewController+Toggle.swift
@@ -11,7 +11,7 @@ import UIKit
 
 extension ProfileViewController {
 
-    func editEnabled() {
+    @objc func editEnabled() {
         editButton.setImage(UIImage(systemName: "square.and.arrow.down"), for: .normal)
         editButton.isSelected = true
 
